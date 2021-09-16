037bd142547e3a8cc681e452b9b80173d2f83f0c
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 15:15:15 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,16 +7,6 @@ import {withPrefix, attribute} from '../utils';
 import Header from './Header';
 import Footer from './Footer';
 
-export default function setDarkMode() {
-    console.log(!!_.get(this.props, 'data.config.color_scheme', null));
-        
-    const [darkMode, isDarkMode] = useState(!!_.get(this.props, 'data.config.color_scheme', null));
-    
-        const buttonHandler = () => {
-            isDarkMode(darkMode => !darkMode);
-        }
-};
-
 export default class Body extends React.Component {
     render() {
         return (
