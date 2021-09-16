8f0e5658caac1099597e5a03111204e0f3fbf3b6
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 13:20:37 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import {htmlToReact} from '../utils';
 import ActionLink from './ActionLink';
 import Action from './Action';
+import Icon from './Icon';
 
 export default class Footer extends React.Component {
     render() {
