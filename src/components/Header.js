09bdb676bacaffb4260a647f23480438f7c2a1aa
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 13:40:25 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,8 @@ export default class Header extends React.Component {
                   </div>
                   {_.get(this.props, 'data.config.header.has_nav', null) && (<React.Fragment>
                       <div>
-                          </div>                  <Icon {...this.props} icon={'dribbble'} />
+                          </div>
+                                            <Icon {...this.props} icon={'dribbble'} />
                   <button id="menu-open" className="menu-toggle"><span className="screen-reader-text">Close Menu</span><span className="icon-menu" aria-hidden="true" /></button>
                   <nav id="main-navigation" className="site-navigation" aria-label="Main Navigation">
                     <div className="site-nav-inside">
