c7c40711968058a9a5e0eb960c6718d14736d5f7
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 13:48:07 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,28 +21,28 @@ export default class Header extends React.Component {
                             }
                         </div>
                         {_.get(this.props, 'data.config.header.has_nav', null) && (
-                        <React.Fragment>
-                            <div className="color-toggle-container">
-                                <Icon {...this.props} icon={'dribbble'} />
-                            </div>
-                            <button id="menu-open" className="menu-toggle"><span className="screen-reader-text">Close Menu</span><span className="icon-menu" aria-hidden="true" /></button>
-                            <nav id="main-navigation" className="site-navigation" aria-label="Main Navigation">
-                                <div className="site-nav-inside">
-                                    <button id="menu-close" className="menu-toggle"><span className="screen-reader-text">Open Menu</span><span className="icon-close" aria-hidden="true" /></button>
-                                    <ul className="menu">
-                                        {_.map(_.get(this.props, 'data.config.header.nav_links', null), (action, action_idx) => {
-                                            let pageUrl = _.trim(_.get(this.props, 'page.stackbit_url_path', null), '/');
-                                            let actionUrl = _.trim(_.get(action, 'url', null), '/');
-                                            return (
-                                                <li key={action_idx} className={classNames('menu-item', { 'current-menu-item': pageUrl === actionUrl, 'menu-button': _.get(action, 'style', null) !== 'link' })}>
-                                                    <Action {...this.props} action={action} />
-                                                </li>
-                                            )
-                                        })}
-                                    </ul>
+                            <React.Fragment>
+                                <div className="color-toggle-container">
+                                    <Icon {...this.props} icon={'dribbble'} />
                                 </div>
-                            </nav>
-                        </React.Fragment>)}
+                                <button id="menu-open" className="menu-toggle"><span className="screen-reader-text">Close Menu</span><span className="icon-menu" aria-hidden="true" /></button>
+                                <nav id="main-navigation" className="site-navigation" aria-label="Main Navigation">
+                                    <div className="site-nav-inside">
+                                        <button id="menu-close" className="menu-toggle"><span className="screen-reader-text">Open Menu</span><span className="icon-close" aria-hidden="true" /></button>
+                                        <ul className="menu">
+                                            {_.map(_.get(this.props, 'data.config.header.nav_links', null), (action, action_idx) => {
+                                                let pageUrl = _.trim(_.get(this.props, 'page.stackbit_url_path', null), '/');
+                                                let actionUrl = _.trim(_.get(action, 'url', null), '/');
+                                                return (
+                                                    <li key={action_idx} className={classNames('menu-item', { 'current-menu-item': pageUrl === actionUrl, 'menu-button': _.get(action, 'style', null) !== 'link' })}>
+                                                        <Action {...this.props} action={action} />
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </React.Fragment>)}
                     </div>
                 </div>
             </header>
