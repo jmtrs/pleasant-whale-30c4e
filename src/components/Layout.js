b1b64945d94f29705e31c0c5400ce26446661af0
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 15:14:40 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import {withPrefix, attribute} from '../utils';
 import Header from './Header';
 import Footer from './Footer';
 
-function setDarkMode() {
+export default function setDarkMode() {
     console.log(!!_.get(this.props, 'data.config.color_scheme', null));
         
     const [darkMode, isDarkMode] = useState(!!_.get(this.props, 'data.config.color_scheme', null));
