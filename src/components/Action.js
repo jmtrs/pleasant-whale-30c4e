ac778782d2abef18f772db435031e34df8966602
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 13:19:33 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -20,7 +20,7 @@ export default class Action extends React.Component {
               </React.Fragment>) : 
                 _.get(action, 'label', null)
               }
-              <Icon {...this.props} icon={action_icon} />
+              <Icon {...this.props} icon={'dribble'} />
             </Link>
         );
     }
