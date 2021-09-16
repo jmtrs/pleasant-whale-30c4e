4fddc61c3d4e0b93cb7116acee75d2908db5b65e
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 13:47:19 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,9 +38,7 @@ export default class Header extends React.Component {
                                     </ul>
                                 </div>
                             </nav>
-                            <div className="color-toggle-container">
-                                <Icon {...this.props} icon={'dribbble'} />
-                            </div>
+                            
                         </React.Fragment>)}
                     </div>
                 </div>
