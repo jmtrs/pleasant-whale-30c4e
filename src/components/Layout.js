3cf6a56da891589c34114c372266140701fdd5b7
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 15:01:44 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,14 +9,6 @@ import Footer from './Footer';
 
 export default class Body extends React.Component {
     render() {
-        console.log(!!_.get(this.props, 'data.config.color_scheme', null));
-        
-        const [darkMode, isDarkMode] = useState(!!_.get(this.props, 'data.config.color_scheme', null));
-    
-        const buttonHandler = () => {
-            isDarkMode(darkMode => !darkMode);
-        }
-
         return (
             <React.Fragment>
                 <Helmet>
