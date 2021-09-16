22c5c67fa6c3eaebd8e5fe7b62cc4b7181b522c4
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 15:00:47 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,11 +11,11 @@ export default class Body extends React.Component {
     render() {
         console.log(!!_.get(this.props, 'data.config.color_scheme', null));
         
-        const [darkMode, isDarkMode] = useState(!!_.get(this.props, 'data.config.color_scheme', null));
+/*         const [darkMode, isDarkMode] = useState(!!_.get(this.props, 'data.config.color_scheme', null));
     
         const buttonHandler = () => {
             isDarkMode(darkMode => !darkMode);
-        }
+        } */
 
         return (
             <React.Fragment>
