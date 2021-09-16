5004ce113303d2454502f3aae696326c91aea074
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 15:18:42 +0000

    setDarkMode.js: changed

--- src/utils/setDarkMode.js
+++ src/utils/setDarkMode.js
@@ -3,8 +3,7 @@ const [darkMode, isDarkMode] = useState(!!_.get(this.props, 'data.config.color_s
 export default function setDarkMode() {
     console.log(!!_.get(this.props, 'data.config.color_scheme', null));
     
-        isDarkMode(darkMode => !darkMode);
-    
+    isDarkMode(darkMode => !darkMode);
 };
 
 export default function getDarkMode() {
