87ab87f2a84c0e1c55c3268d7789355f70e8c6f4
Stackbit Code Editor <projects@stackbit.com>
2021-09-16 15:07:13 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,13 @@ import Header from './Header';
 import Footer from './Footer';
 
 function attribute(name, value, condition) {
+            console.log(!!_.get(this.props, 'data.config.color_scheme', null));
+        
+        const [darkMode, isDarkMode] = useState(!!_.get(this.props, 'data.config.color_scheme', null));
+    
+        const buttonHandler = () => {
+            isDarkMode(darkMode => !darkMode);
+        }
     if (typeof condition === 'undefined') {
         condition = true;
     }
